ac1e8a4df71133a74b50201de6a3d1ec908c539e
Luis Eduardo <luiseduardoalencarmelo@gmail.com>
2025-05-05 12:04:38 -0300

    alter migration

--- src/main/resources/db/migration/V4__lucia_idea_change_user_to_company.sql
+++ src/main/resources/db/migration/V4__lucia_idea_change_user_to_company.sql
@@ -1,6 +1,13 @@
 ALTER TABLE lucia_idea
-    DROP COLUMN IF EXISTS user_id;
+DROP COLUMN IF EXISTS user_id;
 
--- Adiciona o novo campo company_id
 ALTER TABLE lucia_idea
-    ADD COLUMN company_id BIGINT NOT NULL;
+    ADD COLUMN company_id BIGINT;
+
+
+UPDATE lucia_idea
+SET company_id = 1
+WHERE company_id IS NULL;
+
+ALTER TABLE lucia_idea
+    ALTER COLUMN company_id SET NOT NULL;
